8ad69b01a007df162835f0cad161a53094435376
Roman <rasamaxa1905@gmail.com>
2021-01-04 03:15:16 +0300

    hw_test

--- src/hw/Main.java
+++ src/hw/Main.java
@@ -2,6 +2,7 @@ package hw;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hell World");
+        System.out.println("Hello World");
+        System.out.println("Java like");
     }
 }
